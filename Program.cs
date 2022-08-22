c55c030b780685b1b9ebe4b386c2f11cfbfecf10
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-08-21 17:55:53 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WK4Lab1___Movie_Guide___Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
